2cb860e80d371a44be96cb74261b27df4e496276
Shana Hoehn <shanahoehn@gmail.com>
2021-02-04 00:32:10 -0800

    hi

---
+++ content.js
@@ -0,0 +1,43 @@
+// import pizza as '/pizza.png';
+// import pizza from '.file!/pizza.png';
+
+// console.log(pizza);
+const img = document.createElement('img'); //<img src = 'pizza.png'> 
+img.src = './pizza.png'; 
+
+// Array.from(element.attributes).forEach(attr => {
+//   if (`${attr.valueinput}`);
+//  })
+
+//get the input from the form in the popup.html
+const input = document.getElementById("myInput").value; //error: cannot get property value of null
+console.log(input)
+
+// we need to iterate through the alt atribute to see if the input is included
+for (let i = 0; i < elem.attributes.length; i++) {
+  let attrib = elem.attributes[img];
+  if (attrib.matches(`${input}`)
+}
+// if alt attribute includes input, replace img with our pizza image 
+$("[testdata]").each(function(){  }
+
+//save the image to block as a variable
+let imageToBlock = $('img[alt="`${input}`"]');
+
+//set imageToBlock as a parent to save location
+// let parentImage = imageToBlock.parent();
+
+//replace the current image with the new pizza image
+document.body.replaceChild(pizza, imageToBlock);
+//parent                new child   old child
+
+//parentNode.replaceChild(newChild, oldChild);
+
+//let imgElement = document.querySelector('img[alt="the input value"]')
+
+//blockedImage.parentNode.removeChild(blockedImage)
+//.prepend(inserts the specified content as the first child of each element in the jQuery collection)
+
+
+//render
+
